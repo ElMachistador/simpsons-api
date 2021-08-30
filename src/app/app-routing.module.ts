89c8f7ac60f871e63e604599e0f46338b2e87442
ElMachistador <vivelerockpoint0@hotmail.fr>
2021-08-30 13:41:53 +0200

    component parent to child

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { QuotesComponent } from './quotes/quotes.component';
 
 const routes: Routes = [
 
